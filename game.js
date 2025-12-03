9c6ac3aba765d8cfca0e9f83437b894031857376
Tatzzelwurm <131162981+Tatzzelwurm@users.noreply.github.com>
2025-12-03 17:25:40 +0700

    feat: Create function "playRound" and make "humanChoice" parameter case-insensitive

--- game.js
+++ game.js
@@ -4,18 +4,19 @@ let computerScore = 0;
 
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3)
+    
 }
 if (getComputerChoice() == 0) {
-    console.log("Rock");
+    //console.log("Rock");
 
 }
 else if (getComputerChoice() == 1) {
-    console.log("Paper");
+    //console.log("Paper");
     
 }
 else if (getComputerChoice() == 2) {
-    console.log("Scissors");
+    //console.log("Scissors");
     
 }
 
@@ -23,4 +24,11 @@ function getHumanChoice() {
     return prompt("Rock, paper or scissors?");
 }
 
-console.log(getHumanChoice())
+// console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    return humanChoice.at(0).toUpperCase() + humanChoice.slice(1).toLowerCase()
+    
+}
+
+console.log(playRound(getHumanChoice(),getComputerChoice()))
